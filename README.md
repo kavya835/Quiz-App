b755c48f53e1347eb0d5ecea4ef26ba3b1c3f767
Kavya <kavya835282@gmail.com>
2025-03-30 19:07:43 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Quiz App
+Quiz yourself and test your knowledge! Built with Vite + React, this app provides a fast and seamless experience with instant feedback, customizable quizzes, and score tracking. Whether you're a student reviewing for exams or just looking to have fun, this app makes learning enjoyable!
+
+## Features
+* Multiple-choice quizzes
+* Instant feedback
+* Score tracking
+* User-friendly and responsive design
+
+## Installation
+**1. Clone this repository:**
+```
+git clone https://github.com/kavya835/Quiz-App.git
+cd quiz-app
+```
+**2. Install dependencies:**
+```
+npm install
+```
+
+## Running the Program
+```
+npm run dev
+```
---
+++ src/assets/data.js
@@ -0,0 +1,42 @@
+export const data = [
+    {
+        question: "How many bytes is a pointer in a 64-bit system?",
+        option1: "8 bytes",
+        option2: "16 bytes",
+        option3: "4 bytes",
+        option4: "64 bytes",
+        ans: 1,
+    },
+    {
+        question: "What is a[i] in pointer arithmetic format?",
+        option1: "*a[i]",
+        option2: "&(*a) + i",
+        option3: "*(&a[0] + i)",
+        option4: "*((&a)[i])",
+        ans: 3,
+    },
+    {
+        question: "Which of the following definitions is FALSE?",
+        option1: "Testing Harness: using input to call functions",
+        option2: "Black Box Testing: you cannot see the code being tested",
+        option3: "White Box Testing: you can see the code being tested",
+        option4: "Assertion Testing: testing one piece at a time",
+        ans: 4,
+    },
+    {
+        question: "How do you define a string literal 'Hello World!' into a pointer j?",
+        option1: "string j = 'Hello World!'",
+        option2: "char j[] = 'Hello World!'",
+        option3: "char *j[] = 'Hello World!'",
+        option4: "string *j[] = 'Hello World!'",
+        ans: 3,
+    },
+    {
+        question: "Why can we not use <= to compare strings a and b?",
+        option1: "Strings don't have numerical values to compare",
+        option2: "It compares the addresses, not the contents",
+        option3: "The strings a and b have different types",
+        option4: "We can use it, but it is usually not used",
+        ans: 2,
+    },
+];
